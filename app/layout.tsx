720f3f023f18cdfcb13e331beb7704ff3d46ea13
Leonardo Coldibelli <leobragac@hotmail.com>
2026-05-13 14:41:29 -0300

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Granafy.app",
   description: "Granafy.app",
+  icons: {
+    icon: "/logo.jpg",
+  },
 };
 
 export default function RootLayout({
